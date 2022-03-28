b0ae424ddc8c4c2259f7b91c9958d81d0b264a73
Paul Mazaitis <paul@mazaitis.org>
2022-03-28 00:42:19 +0000

    main area now uses markers, not hierarchy

--- grammar.js
+++ grammar.js
@@ -1,6 +1,12 @@
 // Note: the escaped backslash _must_ be the last character in this array (ug)
 var escaped_chars = ['#', '$', '%', '&', '^', '_', '{', '}', '|', '~', '\\'];
 
+var group_start = ["{", "\\start"]; // TODO: Use keyword extraction for this?
+
+var group_stop = ["{", "\\stop"];
+
+// _extras: $ => [" ", "\t", "\n"];
+
 // notes - Handling folds
 //
 // we want to have a notion of foldable nodes - 
@@ -12,19 +18,35 @@ module.exports = grammar({
   rules: {
     // The production rules of the context-free grammar for the ConTeXt markup language
 
-    document: $ => repeat($._content),
+    document: $ => repeat1($._content),
     
-    _content: $ => choice($.comment, $.escaped, $.group, $.text, $.command, $._newline),
+    // AREA MARKERS
+       
+    main_start: $ => prec(2, choice("\\starttext","\\startcomponent")), 
+    
+    main_stop: $ => prec(2, choice("\\stoptext","\\stopcomponent")),   
+       
+    // CONTENT COLLECTIONS
+    
+    _content: $ => choice($.comment, $.escaped, $.group, $.text, $.command, $._newline, $.main_start, $.main_stop),
     
     _opcontent: $ => choice($.comment, $.escaped, $.group, $.optext, $.command, $._newline),
+
+    // COMMENTS
     
     comment: $ => prec(1, token(seq('%', /.*/))),
+
+    // ESCAPED CHARACTERS
 
     escapechar: $ => choice(...escaped_chars),
 
     escaped: $ => prec(1, seq('\\', $.escapechar)),
 
+    // GROUPS
+
     group: $ => prec(1, seq('{', repeat($._content), '}')),
+    
+    // COMMANDS
     
     command: $ => prec(1, seq('\\', $.name, repeat(seq(optional(/\s+/), $.optionblock)), optional(/\s+/))),
     
@@ -37,7 +59,7 @@ module.exports = grammar({
     opval: $ => repeat1($._opcontent),
     
     // We have to double the slashes at the end of the regexp to account for the under-interpolation of escape in this context
-    text: $ => new RegExp('[^\\[\\]'+escaped_chars.slice(1).join('')+'\\]+'),
+    text: $ => new RegExp('[^\\]\\['+escaped_chars.slice(1).join('')+'\\]+'),
     
     optext: $ => /[^\\{}\[\]\s,][^\\{}\[\],]*/,
     
@@ -45,5 +67,9 @@ module.exports = grammar({
  
     _newline: $ => prec(1, '\n'),
     
-  }
+  },
+  
+  // conflicts: $ => [
+  //   [$.preamble, $.main_none]
+  // ]
 });
