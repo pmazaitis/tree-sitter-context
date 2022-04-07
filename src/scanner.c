eb69894f5304e4d2df1e90c8212412060276df0d
Paul Mazaitis <paul@mazaitis.org>
2022-04-07 16:17:53 +0000

    revert from UNUSED trick

--- src/scanner.c
+++ src/scanner.c
@@ -1,17 +1,15 @@
 #include <tree_sitter/parser.h>
 #include <wctype.h>
 
-#define UNUSED(x) (void)(x)
-
 enum TokenType {
   COMMAND_STOP
 };
 
 void *tree_sitter_context_external_scanner_create() { return NULL; }
-void tree_sitter_context_external_scanner_destroy(void *p) {UNUSED(p);}
-void tree_sitter_jcontext_external_scanner_reset(void *p) {UNUSED(p);}
-unsigned tree_sitter_context_external_scanner_serialize(void *p, char *buffer) { UNUSED(p); UNUSED(buffer); return 0; }
-void tree_sitter_context_external_scanner_deserialize(void *p, const char *b, unsigned n) {UNUSED(p); UNUSED(b); UNUSED(n);}
+void tree_sitter_context_external_scanner_destroy(void *p) {}
+void tree_sitter_jcontext_external_scanner_reset(void *p) {}
+unsigned tree_sitter_context_external_scanner_serialize(void *p, char *buffer) { return 0; }
+void tree_sitter_context_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
 static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 static void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
@@ -52,8 +50,6 @@ static bool scan_command_stop(TSLexer *lexer) {
 }
 
 bool tree_sitter_context_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
-
-  UNUSED(payload);
 
   if (valid_symbols[COMMAND_STOP]) {
     return scan_command_stop(lexer);
