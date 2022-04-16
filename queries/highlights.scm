9a120b400aeca8401fb4275672400706c12853e0
Paul Mazaitis <paul@mazaitis.org>
2022-04-16 13:13:31 +0000

    fix highlights (todo: put explicit markers back?)

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,24 +1,24 @@
 ; highlights.scm for tree-sitter-context
 
 (text) @user.text
-(title_value)  @user.text
-(title_text) @user.text
+(title_setting (value (value_brace_group (value_text)))) @user.text
+(subtitle_setting (value (value_brace_group (value_text)))) @user.text
 
 (command_name) @command.name
 
-(setting) @command.set
-(keyword) @command.set
+(settings_block) @command.set
+(option_block) @command.set
 
-(brace_group_start) @group.marker
-(brace_group_stop) @group.marker
-(command_group_start) @group.marker
-(command_group_stop) @group.marker
-(main_start) @group.marker
-(main_stop) @group.marker
-(command_block_start) @group.marker
-(command_block_stop) @group.marker
+;(brace_group_start) @group.marker
+;(brace_group_stop) @group.marker
+;(command_group_start) @group.marker
+;(command_group_stop) @group.marker
+;(main_start) @group.marker
+;(main_stop) @group.marker
+;(command_block_start) @group.marker
+;(command_block_stop) @group.marker
 
-(comment) @comment
+(line_comment) @comment
 
 (escaped) @escaped
 
